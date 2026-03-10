6c5584b7ec4d7b8f5e7b64f13a6c49323a0556ad
Mario Fernando <roshane.ishara@gmail.com>
2026-03-10 21:18:11 +1300

    bump to version 12

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # Working version
-    # return "Hello from version 10"
+    return "Hello from version 12"
 
     # New version with simulated failure
-    if random.random() < 0.5:  # 50% chance to fail
-        return "simulated failure", 500
-    return "Hello from version 11"
+    # if random.random() < 0.5:  # 50% chance to fail
+    #     return "simulated failure", 500
+    # return "Hello from version 11"
 
 @app.route("/health")
 def health():
