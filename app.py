e866fe0570f63efff5ea3cc80566ca31d7443951
Mario Fernando <roshane.ishara@gmail.com>
2026-03-11 14:00:34 +1300

    Bump version 17

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     # Working version
-    return "Hello Auckland k8s Demo - From version 16"
+    return "Hello Auckland k8s Demo - From version 17"
 
     # New version with simulated failure
     # if random.random() < 0.5:  # 50% chance to fail
